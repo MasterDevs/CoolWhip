afab35c380756462b477caacd2f772afdcefad8b
Joe Rall <Joseph.Rall@Live.com>
2015-04-30 20:32:38 -0400

    Implemented very basic Whippet task.

--- source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/Whippet.cs
+++ source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/Whippet.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.Build.Utilities;
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
 
 namespace MasterDevs.CoolWhip
 {
     public class Whippet : Task
     {
+        [Required]
+        public string Version { get; set; }
+
+        [Required]
+        public string TempAssemblyFile { get; set; }
+
         public override bool Execute()
         {
-            Log.LogMessage("I ran a task today and it was liberating");
-            //Log.LogError("Fail");
+            File.WriteAllText(TempAssemblyFile, GetAssemblyVersion());
+            
             return true;
-        }
+        }           
+
+        private string GetAssemblyVersion()
+        {
+            return string.Format(@"[assembly: AssemblyVersion(""{0}"")]", Version ?? "0.0.0.0");
+        }   
     }
 }
