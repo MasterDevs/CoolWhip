53d2cae5e36d5711e1427dc8de427938edafe0c2
Joe Rall <Joseph.Rall@Live.com>
2015-06-21 14:28:22 -0400

    Ok. Made sure we're not copying extra files as a result of them being in the content folder. Added a read me.

--- source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/NugetPackage/tools/install.ps1
+++ source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/NugetPackage/tools/install.ps1
@@ -11,12 +11,12 @@ param($installPath, $toolsPath, $package, $project)
 
 $packagePath = Join-Path $toolsPath ".."
 $projectPath = Split-Path -Parent $project.FullName
-$nuspecTemplatePath = Join-Path $packagePath "content\nuspecTemplate.txt"
+$nuspecTemplatePath = Join-Path $toolsPath "templates\nuspecTemplate.txt"
 $nuspectOutputPath = Join-Path $projectPath "$($project.Name).nuspec"
 $solutionFolder = Split-Path $dte.Solution.FullName
 $gitRoot = findGitRoot -pathInGit $solutionFolder
 $appVeyorOutputPath = Join-Path $gitRoot "appveyor.yml"
-$appVeyorTemplatePath = Join-Path $packagePath "content\appveyor.yml"
+$appVeyorTemplatePath = Join-Path $toolsPath "templates\appveyor.yml"
 $appVeyorContent = Get-Content $appVeyorTemplatePath
 $fullProjectOutputPath = Join-Path $projectPath (Get-Project).ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value
 $relativeProjectOutputPath = $fullProjectOutputPath.Replace($solutionFolder, "")
@@ -48,4 +48,6 @@ Write-Host "Created " $appVeyorOutputPath
 #-- Write the nuspec file for the project
 Copy-Item $nuspecTemplatePath $nuspectOutputPath
 Write-Host "Created " $nuspectOutputPath
+
+#-- Remove the files that were copied as a result
 
