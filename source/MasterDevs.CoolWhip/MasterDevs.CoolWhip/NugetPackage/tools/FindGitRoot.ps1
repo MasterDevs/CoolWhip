2232d64d7bc41fb3a8dd28c61a60d5c6fdcf3dac
Joe Rall <Joseph.Rall@Live.com>
2015-06-21 13:19:51 -0400

    Ok. First round of getting a development env setup for CoolWhip.

---
+++ source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/NugetPackage/tools/FindGitRoot.ps1
@@ -0,0 +1,15 @@
+﻿Function findGitRoot([string]$pathInGit){
+
+	while(![string]::IsNullOrEmpty($pathInGit)) {
+
+		#Check if .git folder exists
+		if(Test-Path "$($pathInGit)\.git") {
+			Write-Host $pathInGit -ForegroundColor DarkCyan
+			Return $pathInGit
+		}
+
+		$pathInGit =  Split-Path -Path $pathInGit -Parent;
+	}
+
+	Return "Ooops"
+}
--- source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/NugetPackage/tools/install.ps1
+++ source/MasterDevs.CoolWhip/MasterDevs.CoolWhip/NugetPackage/tools/install.ps1
@@ -1,20 +1,33 @@
-﻿param($installPath, $toolsPath, $package, $project)
-#https://github.com/jonnii/BuildDeploySupport/blob/master/package/tools/Init.ps1
+﻿#https://github.com/jonnii/BuildDeploySupport/blob/master/package/tools/Init.ps1
 #https://robdmoore.id.au/blog/2013/08/07/test-harness-for-nuget-install-powershell-scripts-init-ps1-install-ps1-uninstall-ps1/
 # AKA https://github.com/robdmoore/NuGetCommandTestHarness
 
-#TODOs -> Add Nuspec File
+
+param($installPath, $toolsPath, $package, $project)
+
+#Bring in some helpers
+. "$($toolsPath)\FindGitRoot.ps1"
+
+
+#TODOs -> Nuspec File
+
+
 
 $packagePath = Join-Path $toolsPath ".."
 $projectPath = Split-Path -Parent $project.FullName
 
+Write-Host $project.Name -ForegroundColor DarkYellow
+
+
 $solutionFolder = Split-Path $dte.Solution.FullName
-$appveyorPath = Join-Path $packagePath "content\appveyor.yml"
-$appVeyorOutputPath = Join-Path $solutionFolder "appveyor.yml"
-$appVeyorContent = Get-Content $appveyorPath
+$gitRoot = findGitRoot -pathInGit $solutionFolder
+$appVeyorOutputPath = Join-Path $gitRoot "appveyor.yml"
+$appVeyorTemplatePath = Join-Path $packagePath "content\appveyor.yml"
+$appVeyorContent = Get-Content $appVeyorTemplatePath
 $fullProjectOutputPath = Join-Path $projectPath (Get-Project).ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value
 
-#Replace Artifact Output Paths
+
+#Replace Artificat Output Paths
 $relativeProjectOutputPath = $fullProjectOutputPath.Replace($solutionFolder, "")
 $appVeyorContent = $appVeyorContent.Replace("{{relateProjectOutputPath}}", $relativeProjectOutputPath)
 $appVeyorContent = $appVeyorContent.Replace("{{artifactName}}", $project.Name)
@@ -23,12 +36,11 @@ $appVeyorContent = $appVeyorContent.Replace("{{artifactName}}", $project.Name)
 $appVeyorContent = $appVeyorContent.Replace("{{assemblyInfoRelativePath}}", $projectPath.Replace($solutionFolder, ""))
 
 #Write AppVeyor file (if it doesn't exist)
-if(Test-Path $appVeyorOutputPath) 
-{
-	Write-Host "File Exists!" -ForegroundColor Red
+if(Test-Path $appVeyorOutputPath) {
+	Write-Host "Not overwriting File Exists!" -ForegroundColor Red
+	Write-Error "Could not overwrite file"
 }
-else 
-{
+else {
 	Set-Content $appVeyorOutputPath $appVeyorContent
 	Write-Host "Created " $appVeyorOutputPath -ForegroundColor DarkCyan
 }
